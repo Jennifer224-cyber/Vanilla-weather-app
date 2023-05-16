026d8d407430cb556a7534a3cc2c446514edc713
Jennifer <jenniferneliswa313@gmail.com>
2023-05-16 23:41:27 +0200

    integrated axios and api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-alert("Hello world!");
+let apiKey = "b2a083t25a3aoc4a993bbafe88f5406b";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query={Johannesburg}&key={apiKey}&units=metric";
+console.log(apiUrl);
