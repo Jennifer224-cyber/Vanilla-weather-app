754654744bc1608e89286127e7ef703e09075bdb
Jennifer <jenniferneliswa313@gmail.com>
2023-05-18 19:10:30 +0200

    updated javascript current date and time function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours} : ${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let today = document.querySelector("#currentTime");
-today.innerHTML = `${day} ${hour} : ${minutes}`;
 
 function displayTemperature(response) {
   document.querySelector("#currentTemperature").innerHTML = Math.round(
@@ -31,8 +33,11 @@ function displayTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#currentTime").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
-let city = "Tokyo";
+let city = "New York";
 let key = "012989fa51ba761977563de59d09f69c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
