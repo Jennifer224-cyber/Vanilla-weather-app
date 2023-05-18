d4b02803f08d2ed124c305f126b2b3d365ec0cda
Jennifer <jenniferneliswa313@gmail.com>
2023-05-18 16:36:26 +0200

    showing current temperature, day and time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-let apiKey = "b2a083t25a3aoc4a993bbafe88f5406b";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={Johannesburg}&key={apiKey}&units=metric";
-console.log(apiUrl);
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let today = document.querySelector("#currentTime");
+today.innerHTML = `${day} ${hour} : ${minutes}`;
+
+function displayTemperature(response) {
+  document.querySelector("#currentTemperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#currentDescription").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+let city = "Tokyo";
+let key = "012989fa51ba761977563de59d09f69c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
